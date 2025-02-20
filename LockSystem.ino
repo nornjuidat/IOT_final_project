3e4b9d37df65b25d748b5762c84f494e13c2c138
נור <njuidatnor750@gmail.com>
2025-02-20 14:23:59 +0200

    LockSystem.ino
    הגדרת משתנים ל בקר הנעילה (ESP8266), ומטרתו היא לנהל את מנגנון פתיחת הדלת דרך דף אינטרנט ולשלוט

---
+++ LockSystem.ino
@@ -0,0 +1,17 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include "WiFiSetup.h"
+
+#define LOCK_PIN D8
+#define DISPLAY_PINS {D1, D2, D3, D4}
+
+ESP8266WebServer server(80);
+String accessCode = "1234";
+String enteredCode = "";
+int currentDigit = 0;
+
+void openLock() {
+    digitalWrite(LOCK_PIN, HIGH);
+    delay(5000);
+    digitalWrite(LOCK_PIN, LOW);
+}
