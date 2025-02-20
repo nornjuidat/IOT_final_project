a9da223703b9cb6a86b87ccf2f2f95386510d109
נור <njuidatnor750@gmail.com>
2025-02-20 14:20:33 +0200

    LockController.ino
    הגדרת משתמים עבור בקר הנעילה

---
+++ LockController.ino
@@ -0,0 +1,17 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <WebSocketsServer.h>
+#include <SPI.h>
+#include <Wire.h>
+#include "Adafruit_GFX.h"
+#include "Adafruit_LEDBackpack.h"
+#include "WiFiConfig.h"
+
+#define LOCK_PIN D1  // פין לנעילה
+ESP8266WebServer server(80);
+WebSocketsServer webSocket(81);
+Adafruit_7segment display = Adafruit_7segment();
+
+String correctCode = "1234"; 
+String enteredCode = "";
+String revealedDigits = "____";
