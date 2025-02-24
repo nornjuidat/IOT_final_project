c90525ca931a0cc2bf42d10f8f9d176cc35d1ad6
mhmdkhateeb74 <mhmdkhateeb92@gmail.com>
2025-02-24 19:59:24 +0200

    Library
    ESP8266WiFi.h: מאפשר ל-ESP8266 להתחבר לרשת WiFi.
    ESP8266WebServer.h: יוצר שרת אינטרנט לטיפול בבקשות HTTP.
    WebSocketsServer.h: מאפשר תקשורת בזמן אמת באמצעות WebSockets.
    SPI.h & Wire.h: משמשים לפרוטוקולי תקשורת (SPI & I2C) הנדרשים עבור התצוגה.
    Adafruit_GFX.h & Adafruit_LEDBackpack.h: ספריות לשליטה בתצוגת 7-סגמנטים.
    WiFiConfig.h: קובץ נפרד (שאינו מוצג כאן) שמכיל ככל הנראה פרטי חיבור ל-WiFi והגדרות רשת.

---
+++ LockController.ino
@@ -0,0 +1,8 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <WebSocketsServer.h>
+#include <SPI.h>
+#include <Wire.h>
+#include "Adafruit_GFX.h"
+#include "Adafruit_LEDBackpack.h"
+#include "WiFiConfig.h"
